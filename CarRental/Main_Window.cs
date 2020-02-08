7dafde444ba331730bc1e97faaab9aa7afecc63a
adrian3xl <59806075+adrian3xl@users.noreply.github.com>
2020-02-08 01:23:45 -0500

    Updated to allow for data to be stored from winform to database

---
+++ CarRental/Main_Window.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarRental
+{
+    public partial class Main_Window : Form
+    {
+        public Main_Window()
+        {
+            InitializeComponent();
+        }
+
+        private void addToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 Form1 = new Form1();
+            Form1.MdiParent = this;
+            Form1.Show();
+        }
+    }
+}
