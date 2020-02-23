57bb5c0407db41b903379067959fd90d86dca191
adrian3xl <59806075+adrian3xl@users.noreply.github.com>
2020-02-23 14:24:57 -0500

    update employers view

--- JobApp/Employer_accountview.Designer.cs
+++ JobApp/Employer_accountview.Designer.cs
@@ -43,7 +43,12 @@
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.textBox7 = new System.Windows.Forms.TextBox();
-            this.view_vacancy_bt = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.addVacancy_MenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateVacancyT_MenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewVacancies_MenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainWindow_MenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -51,7 +56,7 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(130, 13);
+            this.label1.Location = new System.Drawing.Point(534, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(244, 39);
             this.label1.TabIndex = 13;
@@ -61,7 +66,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(380, 29);
+            this.label5.Location = new System.Drawing.Point(284, 109);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(111, 17);
             this.label5.TabIndex = 17;
@@ -69,7 +74,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(493, 29);
+            this.textBox2.Location = new System.Drawing.Point(421, 106);
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
             this.textBox2.Size = new System.Drawing.Size(189, 20);
@@ -79,7 +84,7 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(667, 77);
+            this.label9.Location = new System.Drawing.Point(256, 165);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(178, 17);
             this.label9.TabIndex = 23;
@@ -89,7 +94,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(134, 77);
+            this.label3.Location = new System.Drawing.Point(304, 255);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(130, 17);
             this.label3.TabIndex = 24;
@@ -99,7 +104,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(111, 172);
+            this.label4.Location = new System.Drawing.Point(894, 76);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(153, 17);
             this.label4.TabIndex = 25;
@@ -109,7 +114,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(769, 9);
+            this.label8.Location = new System.Drawing.Point(335, 303);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(99, 17);
             this.label8.TabIndex = 29;
@@ -119,7 +124,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(667, 109);
+            this.label6.Location = new System.Drawing.Point(324, 213);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(110, 17);
             this.label6.TabIndex = 27;
@@ -129,7 +134,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(989, 9);
+            this.label7.Location = new System.Drawing.Point(300, 363);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(134, 17);
             this.label7.TabIndex = 28;
@@ -137,7 +142,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(740, 32);
+            this.textBox1.Location = new System.Drawing.Point(440, 300);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(159, 20);
@@ -145,15 +150,15 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(974, 32);
+            this.textBox3.Location = new System.Drawing.Point(440, 360);
             this.textBox3.Name = "textBox3";
             this.textBox3.ReadOnly = true;
-            this.textBox3.Size = new System.Drawing.Size(159, 20);
+            this.textBox3.Size = new System.Drawing.Size(63, 20);
             this.textBox3.TabIndex = 31;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(270, 118);
+            this.textBox4.Location = new System.Drawing.Point(817, 109);
             this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
             this.textBox4.ReadOnly = true;
@@ -162,7 +167,7 @@
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(270, 77);
+            this.textBox5.Location = new System.Drawing.Point(440, 255);
             this.textBox5.Multiline = true;
             this.textBox5.Name = "textBox5";
             this.textBox5.ReadOnly = true;
@@ -171,7 +176,7 @@
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(852, 109);
+            this.textBox6.Location = new System.Drawing.Point(440, 210);
             this.textBox6.Name = "textBox6";
             this.textBox6.ReadOnly = true;
             this.textBox6.Size = new System.Drawing.Size(159, 20);
@@ -179,29 +184,60 @@
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(851, 74);
+            this.textBox7.Location = new System.Drawing.Point(440, 165);
             this.textBox7.Name = "textBox7";
             this.textBox7.ReadOnly = true;
             this.textBox7.Size = new System.Drawing.Size(159, 20);
             this.textBox7.TabIndex = 35;
             // 
-            // view_vacancy_bt
+            // menuStrip1
             // 
-            this.view_vacancy_bt.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.view_vacancy_bt.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.view_vacancy_bt.Location = new System.Drawing.Point(740, 205);
-            this.view_vacancy_bt.Name = "view_vacancy_bt";
-            this.view_vacancy_bt.Size = new System.Drawing.Size(283, 50);
-            this.view_vacancy_bt.TabIndex = 42;
-            this.view_vacancy_bt.Text = "Click Here For Vacancies";
-            this.view_vacancy_bt.UseVisualStyleBackColor = false;
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addVacancy_MenuItem,
+            this.updateVacancyT_MenuItem,
+            this.viewVacancies_MenuItem,
+            this.mainWindow_MenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(195, 492);
+            this.menuStrip1.TabIndex = 43;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // addVacancy_MenuItem
+            // 
+            this.addVacancy_MenuItem.Name = "addVacancy_MenuItem";
+            this.addVacancy_MenuItem.Size = new System.Drawing.Size(188, 19);
+            this.addVacancy_MenuItem.Text = "Add Vacancy";
+            // 
+            // updateVacancyT_MenuItem
+            // 
+            this.updateVacancyT_MenuItem.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.updateVacancyT_MenuItem.Name = "updateVacancyT_MenuItem";
+            this.updateVacancyT_MenuItem.Size = new System.Drawing.Size(188, 19);
+            this.updateVacancyT_MenuItem.Text = "Update Vacancy";
+            // 
+            // viewVacancies_MenuItem
+            // 
+            this.viewVacancies_MenuItem.Name = "viewVacancies_MenuItem";
+            this.viewVacancies_MenuItem.Size = new System.Drawing.Size(188, 19);
+            this.viewVacancies_MenuItem.Text = "View Vacancies of your company";
+            // 
+            // mainWindow_MenuItem
+            // 
+            this.mainWindow_MenuItem.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.mainWindow_MenuItem.Name = "mainWindow_MenuItem";
+            this.mainWindow_MenuItem.Size = new System.Drawing.Size(188, 19);
+            this.mainWindow_MenuItem.Text = "Main Window";
             // 
             // Employer_accountview
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1174, 492);
-            this.Controls.Add(this.view_vacancy_bt);
+            this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.textBox7);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.textBox5);
@@ -219,6 +255,8 @@
             this.Controls.Add(this.label1);
             this.Name = "Employer_accountview";
             this.Text = "Employer_accountview";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -241,6 +279,10 @@
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.Button view_vacancy_bt;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem addVacancy_MenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updateVacancyT_MenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewVacancies_MenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mainWindow_MenuItem;
     }
 }
