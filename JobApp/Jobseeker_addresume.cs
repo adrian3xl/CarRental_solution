f79e295f3cc8f42d80628e6b646e37fdcd358dbc
adrian3xl <59806075+adrian3xl@users.noreply.github.com>
2020-03-11 21:16:43 -0500

    edits

--- JobApp/Jobseeker_addresume.cs
+++ JobApp/Jobseeker_addresume.cs
@@ -6,29 +6,13 @@ namespace JobApp
 
     public partial class Jobseeker_addresume : Form
     {
-        private bool issubmit_mode;
+         
         private readonly Jobapp_dbEntities _db;
         public Jobseeker_addresume()
         {
             InitializeComponent();
             _db = new Jobapp_dbEntities();
 
-        }
-        public Jobseeker_addresume(Job_Applications_details addresume)
-        {
-            InitializeComponent();
-            populateFields(addresume);
-
-        }
-
-        private void populateFields(Job_Applications_details addresume)
-        {
-            edu_tb.Text = addresume.Resume_details.Education_level;
-            cont_tb.Text = addresume.Resume_details.Contact_number;
-            hobby_tb.Text = addresume.Resume_details.Hobbies;
-            email_tb.Text = addresume.Resume_details.Email;
-            exper_tb.Text = addresume.Resume_details.PriorWork_Experiences;
-            qualifica_tb.Text = addresume.Resume_details.Qualifications;
         }
 
         private void Cancel_bt_Click(object sender, EventArgs e)
@@ -74,6 +58,7 @@ namespace JobApp
 
 
                     resume_id = resume_id,
+
                   
 
                 };
